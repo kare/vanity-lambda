02f5d54024673be44c32fcc4a6ba13f3d9f30b4a
Kare Nuorteva <kare.nuorteva@me.com>
2026-01-31 15:51:01 +0200

    refactor: add Lambda function

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/kare/vanity-lambda
 go 1.25.0
 
 require (
-	github.com/aws/aws-lambda-go v1.52.0 // indirect
-	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2 // indirect
-	kkn.fi/vanity v1.0.0 // indirect
+	github.com/aws/aws-lambda-go v1.52.0
+	github.com/awslabs/aws-lambda-go-api-proxy v0.16.2
+	kkn.fi/vanity v1.0.0
 )
--- main.go
+++ main.go
@@ -1,4 +1,33 @@
 package main
 
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/awslabs/aws-lambda-go-api-proxy/httpadapter"
+	"kkn.fi/vanity"
+)
+
 func main() {
+	domain := os.Getenv("VANITY_DOMAIN")
+	vcsUrl := os.Getenv("VANITY_VCSURL")
+
+	opts := []vanity.Option{}
+	if domain != "" {
+		opts = append(opts, vanity.Domain(domain))
+	}
+	if vcsUrl != "" {
+		opts = append(opts, vanity.VCSURL(vcsUrl))
+	}
+
+	h, err := vanity.NewHandlerWithOptions(opts...)
+	if err != nil {
+		log.Fatalf("failed to init vanity handler: %v", err)
+	}
+	handler := http.NewServeMux()
+	handler.Handle("/", h)
+	adapter := httpadapter.New(handler)
+	lambda.Start(adapter.ProxyWithContext)
 }
